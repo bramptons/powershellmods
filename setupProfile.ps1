4151b7be5d420bb5d8a6a9a46835a02cfa5a6358
bramptons <abesphone@live.co.uk>
2017-11-08 14:42:57 +0000

    imperial march

--- setupProfile.ps1
+++ setupProfile.ps1
@@ -13,6 +13,45 @@ if (!(Test-IsAdmin)){
     Write-Output "You aint no admin! check ya self"}
 else{
     $host.ui.RawUI.WindowTitle="Boss Mode"
-    Write-Output "You cool Boss, carry on.."}
+    Write-Output "You cool Boss, carry on.."
+
+    start-job{
+        [console]::beep(440,500)
+        
+        [console]::beep(440,500)
+        
+        [console]::beep(440,500)
+        
+        [console]::beep(349,350)
+        
+        [console]::beep(523,150)
+        
+        [console]::beep(440,500)
+        
+        [console]::beep(349,350)
+        
+        [console]::beep(523,150)
+        
+        [console]::beep(440,1000)
+        
+        [console]::beep(659,500)
+        
+        [console]::beep(659,500)
+        
+        [console]::beep(659,500)
+        
+        [console]::beep(698,350)
+        
+        [console]::beep(523,150)
+        
+        [console]::beep(415,500)
+        
+        [console]::beep(349,350)
+        
+        [console]::beep(523,150)
+        
+        [console]::beep(440,1000)
+        }
+}
 
 set-location c:\
